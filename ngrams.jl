66b621f5fcf36408b4cecda9fbc26bad975617d2
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2023-08-22 22:58:04 +0800

    lcs zip

--- ngrams.jl
+++ ngrams.jl
@@ -49,6 +49,51 @@ function merged_dataset_ngrams(dataset, n)
     counter
 end
 
+function lcs(s1::AbstractString, s2::AbstractString)
+    m, n = length(s1), length(s2)
+    len, pos1, pos2 = 0, 0, 0
+    dp = zeros(Int, m+1, n+1)
+    for i in 1:m
+        for j in 1:n
+            if s1[i] == s2[j]
+                dp[i+1, j+1] = dp[i, j] + 1
+                if dp[i+1, j+1] > len
+                    len = dp[i+1, j+1]
+                    pos1 = i
+                    pos2 = j
+                end
+            end
+        end
+    end
+    return pos1 - len + 1:pos1, pos2 - len + 1:pos2
+end
+function lcs_zip(str, refer)
+    rg1, rg2 = lcs(refer, str)
+    if length(rg1) > 2
+        b1, e1 = first(rg1), last(rg1)
+        b2, e2 = first(rg2), last(rg2)
+        l1 = e1 - b1 + 1
+        if b1 < 26 && l1 < 26
+            bc = Char(Int('A') + b1 - 1)
+            lc = Char(Int('A') + l1 - 1)
+            str = string(str[1:b2-1], bc, lc, str[e2+1:end])
+        end
+    end
+    str
+end
+function lcs_unzip(str, refer)
+    b2 = findfirst(r"[A-Z]", str)
+    if b2 !== nothing
+        b2 = first(b2)
+        e2 = b2 + 1
+        b1 = Int(str[b2]) - Int('A') + 1
+        l1 = Int(str[e2]) - Int('A') + 1
+        e1 = b1 + l1 - 1
+        str = string(str[1:b2-1], refer[b1:e1], str[e2+1:end])
+    end
+    str
+end
+
 function dump_ngrams(D, filename; head=nothing)
     open(filename, "w") do f
         if head !== nothing
@@ -58,16 +103,21 @@ function dump_ngrams(D, filename; head=nothing)
         end
         last_k = "###"
         last_v = 0.0
+        last_vstr = string(last_v)
         for (k, v) in D
             @assert k isa Vector{UInt8}
             k = join(string.(k, base=16), "")
             kz = replace(k, last_k => "+")
+            kz = lcs_zip(kz, last_k)
             last_k = k
             write(f, kz)
             if last_v != v
                 last_v = v
                 write(f, ",")
-                write(f, string(v))
+                vstr = string(v)
+                vstrz = lcs_zip(vstr, last_vstr)
+                last_vstr = vstr
+                write(f, vstrz)
             end
             write(f, "\n")
         end
@@ -84,16 +134,20 @@ function load_ngrams(filename; head=true)
         D = Vector{Pair{Vector{UInt8}, Float64}}()
         last_k = "###"
         last_v = 0.0
+        last_vstr = string(last_v)
         for line in el
             kz_v = split(line, ",")
             if length(kz_v) == 1
                 kz, v = kz_v[1], last_v
             else
-                kz, v = kz_v
-                v = parse(Float64, v)
+                kz, vstrz = kz_v
+                vstr = lcs_unzip(vstrz, last_vstr)
+                last_vstr = vstr
+                v = parse(Float64, vstr)
                 last_v = v
             end
             k = replace(kz, "+" => last_k)
+            k = lcs_unzip(k, last_k)
             last_k = k
             @assert iseven(length(k))
             k = parse.(UInt8, Iterators.partition(string(k), 2), base=16)
