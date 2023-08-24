01834cdf7bda0c80e057dc4af907cff22ea0a028
guoyongzhi <guo-yong-zhi@outlook.com>
2023-08-24 17:17:29 +0800

    detector

--- detector.jl
+++ detector.jl
@@ -1,17 +1,52 @@
 include("ngrams.jl")
-include("datasetloader.jl")
+const PACKAGE_PATH = "."
+const ALL_LANGUAGES = [f[1:end-4] for f in readdir(joinpath(PACKAGE_PATH, "ngrams"))]
+const LANGUAGES = String[]
+const PROFILES = Vector{Dict{Vector{UInt8}, Float32}}()
+DEFAULT_LOGP::Float32 = typemin(Float32)
+NGRAM::Int = 0
 
-const G = [lang => load_ngrams(joinpath("ngrams", lang * ".txt")) for lang in LANGS];
-
-function norm_table!(t)
-    D = last(t)
-    vs = sum(values(D))
-    for (k, v) in D
-        D[k] /= vs
-        D[k] = log(D[k])
-    end
+function get_supported_languages()
+    return ALL_LANGUAGES
 end
-function likelihood(t, logt, default_logp=DEFAULT_LOGP)
+
+function initialize(;languages=get_supported_languages(), ngram=4, cutoff=0.85)
+    empty!(LANGUAGES)
+    append!(LANGUAGES, languages)
+    empty!(PROFILES)
+    for lang in LANGUAGES
+        push!(PROFILES, load_profile(lang, ngram, cutoff))
+    end
+    global DEFAULT_LOGP = minimum(minimum.(values.(PROFILES)))
+    global NGRAM = ngram
+    nothing
+end
+
+function load_profile(lang, ngram, cutoff)
+    hd, rows = ngram_table(joinpath(PACKAGE_PATH, "ngrams", lang * ".txt"))
+    total = sum(hd[1:ngram])
+    cums = 0.0
+    P = Pair{Vector{UInt8}, Float32}[]
+    for (k, v) in rows
+        if length(k) <= ngram
+            cums += v
+            push!(P, k => v)
+            if cums / total >= cutoff
+                break
+            end
+        end
+    end
+    cums / total >= cutoff || @info "$lang: cutoff($cutoff) not reached. current: $(cums / total). size: $(length(P))"
+    normalize_profile!(Dict(P))
+end
+
+function normalize_profile!(P)
+    vs = sum(values(P))
+    map!(v -> log(v / vs), values(P))
+    P
+end
+
+function loglikelihood(t, logt, default_logp=DEFAULT_LOGP)
     sc = 0.0
     for (code, p) in t
         logq = get(logt, code, default_logp)
@@ -19,12 +54,20 @@ function likelihood(t, logt, default_logp=DEFAULT_LOGP)
     end
     sc
 end
-function detector_lh(text; ngram=5, languages=LANGS, logtable=LOG_T)
-    t = merged_ngrams(text, ngram)
-    lhs = likelihood.(Ref(t), logtable)
-    languages[argmax(lhs)]
+
+function langid(text; ngram=NGRAM, languages=LANGUAGES, profiles=PROFILES)
+    p = merged_ngrams(text, ngram)
+    lls = loglikelihood.(Ref(p), profiles)
+    languages[argmax(lls)]
 end
 
-norm_table!.(G);
-const LOG_T = last.(G);
-const DEFAULT_LOGP = (minimum.(values.(last.((G)))) |> minimum)
+function langprob(text; candidates=5, ngram=NGRAM, languages=LANGUAGES, profiles=PROFILES)
+    p = merged_ngrams(text, ngram)
+    vs = sum(values(p))
+    map!(v -> v / vs, values(p))
+    lls = loglikelihood.(Ref(p), profiles)
+    ls = exp.(lls)
+    ls = ls ./ sum(ls)
+    si = sortperm(ls, rev=true)[1:candidates]
+    [k => v for (k, v) in zip(languages[si], ls[si])]
+end
