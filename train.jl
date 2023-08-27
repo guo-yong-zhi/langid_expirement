f6caa53e5c8ef362b7f1088ada7106b222166721
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2023-08-27 23:53:43 +0800

    val loss

--- train.jl
+++ train.jl
@@ -18,7 +18,7 @@ mutable struct Model
     ngram::Int
 end
 
-function Model(path)
+function Model(path::AbstractString)
     langs = [f[1:end-4] for f in readdir(path) if endswith(f, ".txt")]
     langs_inds = Dict(lang => i for (i, lang) in enumerate(langs))
     Qs = Vector{Dict{Vector{UInt8}, Float32}}()
@@ -52,21 +52,20 @@ function loss(lls, ys)
     m = maximum(lls)
     sum(ys .* (log.(sum(exp.(lls .- m))) .+ m .- lls)) # softmax & cross entropy
 end
-function get_loss(params, p::AbstractDict, ys::AbstractVector)
+function get_loss(params, p_ys::Tuple{<:AbstractDict, <:AbstractVector})
+    p, ys = p_ys
     lls = [loglikelihood(p, Q, params.default_q, c)
             for (c, Q) in zip(params.cutoff_list, params.Qs)]
     loss(lls, ys)
 end
-function get_loss(params, batch::AbstractVector{<:AbstractDict}, ys::AbstractMatrix)
-    sum(get_loss.(Ref(params), batch, eachcol(ys))) / size(ys, 2)
+function get_loss(params, batch::AbstractVector{<:Tuple{<:AbstractDict, <:AbstractVector}})
+    sum(get_loss.(Ref(params), batch)) / length(batch)
 end
-function loss_and_grad(params, params_aux, p, ys)
-    val, grad = withgradient(params) do params
-        get_loss(merge(params, params_aux), p, ys)
-    end
-    val, grad[1]
+function get_loss(params, data)
+    get_loss(params, preprocess_data(data, params.ngram))
 end
-function preprocess_data(x::AbstractString, y::AbstractString, ngram)
+function preprocess_data(x_y::Union{Tuple, Pair}, ngram)
+    x, y = x_y
     yi = M.langs_inds[y]
     onehot = zeros(Float32, length(M.languages))
     onehot[yi] = 1.0
@@ -74,18 +73,21 @@ function preprocess_data(x::AbstractString, y::AbstractString, ngram)
     (p, onehot)
 end
 function preprocess_data(data::AbstractVector, ngram)
-    xys = [preprocess_data(x, y, ngram) for (x, y) in data]
-    xs = [xy[1] for xy in xys]
-    ys = hcat((xy[2] for xy in xys)...)
-    xs, ys
+    preprocess_data.(data, Ref(ngram))
+end
+function loss_and_grad(params, params_aux, p_ys)
+    val, grad = withgradient(params) do params
+        get_loss(merge(params, params_aux), p_ys)
+    end
+    val, grad[1]
 end
 function loss_and_grad(model::Model, args...; kwargs...)
-    x, y = preprocess_data(args...; kwargs...)
     params = (Qs=model.Qs, default_q=model.default_q)
     params_aux = (cutoff_list=model.cutoff_list,)
-    loss_and_grad(params, params_aux, x, y)
+    data = preprocess_data(args...; kwargs...)
+    loss_and_grad(params, params_aux, data)
 end
-function step!(model, grad, lr::Float32=0.01f0)
+function step!(model::Model, grad, lr::Float32=0.01f0)
     if grad.default_q !== nothing
         model.default_q -= lr * grad.default_q
     end
@@ -106,7 +108,7 @@ function cutoff_value_by_ratio(tb, ngram, r)
     probs = probs[si]
     cs = cumsum(probs)
     ind = findfirst(x -> x >= r * cs[end], cs)
-    ind == nothing ? typemin(valtype(tb)) : logits[ind]
+    ind === nothing ? typemin(valtype(tb)) : logits[ind]
 end
 function cutoff_value_by_size(m::Model, ngram, s)
     for (i, tb) in enumerate(m.Qs)
@@ -124,8 +126,8 @@ end
 function random_cutoff(m::Model)
     ngram = rand([1:7; 3:7; 3:5])
     if rand() < 0.5
-        cutoff_value_by_size(m, ngram, rand(100:20000))
+        cutoff_value_by_size(m, ngram, rand([100:20000; 20000:10:100000]))
     else
-        cutoff_value_by_ratio(m, ngram, rand(0.5:0.01:1.0))
+        cutoff_value_by_ratio(m, ngram, rand(0.5:0.001:1.0))
     end
 end
